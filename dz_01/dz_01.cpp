70c3f6acf7f2884a3d09a08106ec2703759d6190
Hellhound <hardoneloves@gmail.com>
2021-11-29 18:20:36 +0300

    Добавьте файлы проекта.

---
+++ dz_01/dz_01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+void v(int arr[], int mn) {
+	for (int i = 0; i < mn; i++) std::cout << arr[i] << " ";
+}
+void g(int* a, int b, int n) {
+
+	if (a != nullptr) {
+		for (int i = 0; i < n; i++) {
+			a[i] = b;
+			b = b * 2;
+		}
+	}
+}
+int main() {
+	int* a;
+	int n, b = 2;
+	std::cin >> n;
+	a = new (std::nothrow) int[n];
+	g(a, b, n);
+	v(a, n);
+}
